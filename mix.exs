9b017e377023540ece13fb08641307a0f2ea8e71
Cameron Bytheway <bytheway.cameron@gmail.com>
2016-11-01 13:28:32 -0600

    release 0.1.0

--- mix.exs
+++ mix.exs
@@ -3,11 +3,13 @@ defmodule PlugXForwardedProto.Mixfile do
 
   def project do
     [app: :plug_x_forwarded_proto,
+     description: "x-forwarded-proto plug middleware",
      version: "0.1.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     package: package()]
   end
 
   def application do
@@ -15,6 +17,14 @@ defmodule PlugXForwardedProto.Mixfile do
   end
 
   defp deps do
-    [{:plug, ">= 0.0.0"}]
+    [{:plug, ">= 0.0.0"},
+     {:ex_doc, ">= 0.0.0", only: :dev}]
+  end
+
+  defp package do
+    [files: ["lib", "mix.exs", "README*"],
+     maintainers: ["Cameron Bytheway"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/exstruct/mazurka_plug"}]
   end
 end
